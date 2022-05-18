3bd0ca11c594372391abeee7113da416cad2cfad
Bin <luanhc2901@gmail.com>
2022-05-18 21:21:31 +0700

    view list products with mock data and search products

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Button } from 'react-bootstrap';
 import './styles.scss'
-export default function Card() {
+export default function Card({product}) {
+    // console.log("Product: ", product);
     return (
         <div className="card-product pb-3 pt-2 mx-3 my-5">
-            <img className="image" src='https://philong.com.vn/media/product/24769-10.jpg' alt="" />
+            <img className="image" src={product.image} alt="" />
             <div className="info-product">
-                <h5>LAP ABC</h5>
-                <p>Hang: ASUS</p>
-                <p>Chip: Itel</p>
-                <p>Gia: 10000000 VND</p>
+                
+                <h5>{product.name}</h5>
+                <p>{product.brand}</p>
+                <p>{product.chip}</p>
+                <p>{product.price}</p>
             </div>
             <div className="btn-view">
                 <Button variant="primary">Xem san pham</Button>
